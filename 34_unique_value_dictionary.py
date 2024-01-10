78f08a71cf76a90621c071c3a351a6c9eaa0c26c
Kiyog1676 <139625108+Kiyog1676@users.noreply.github.com>
2024-01-10 07:58:11 -0500

    Delete 34_unique_value_dictionary.py

--- 34_unique_value_dictionary.py
+++
@@ -1,7 +0,0 @@
-dict={"kiran":100,"himanshi":200,"yatin":300,"surbhi":200,"mansi":300,"hemal":200}
-unique_dic={}
-
-for i in dict:
-    if i not in unique_dic:
-        unique_dic.append(dict)
-print("unique dic",unique_dic)
