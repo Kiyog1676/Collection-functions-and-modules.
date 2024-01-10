d45e11ea9fae3524e8e82192840d45df9e604977
Kiyog1676 <139625108+Kiyog1676@users.noreply.github.com>
2024-01-10 07:59:04 -0500

    Delete 38_combain_value_in_dictionary.py

--- 38_combain_value_in_dictionary.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-
-dict = [{'item': 'item1', 'amount': 400}, {'item': 'item2', 'amount': 300}, {'item': 'item1', 'amount': 750}]
-
-d1=Counter()
-
-for i in dict:
-        d1[i['item']] += i['amount']
-print(d1)
